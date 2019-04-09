5d43bb20868d0c4becfa997bd44423c6c597c020
MAKSYM_enko <finside41@gmail.com>
2019-04-09 15:38:17 +0300

    Some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send(`Hello World ! from port: ${port}`));
+app.get("/", (req, res) => res.send(`Hello World! from port: ${port}`));
 
 app.listen(port, () =>
   console.log(`Example app listening on port http://127.0.0.1:${port}!`)
