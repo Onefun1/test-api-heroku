9e3fd064d3826897607cfec1466a6f38da3591ca
MAKSYM_enko <finside41@gmail.com>
2019-04-09 21:58:21 +0300

    Users were move to the server + nodemon

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+let users = [
+  {
+    firstname: "Morgan",
+    lastname: "Freeman"
+  },
+  {
+    firstname: "First",
+    lastname: "Aid"
+  }
+];
+
 app.get("/", (req, res) => res.send(`Hello World! from port: ${port}`));
+app.get("/users", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.json(users);
+});
 
 app.listen(port, () =>
-  console.log(`Example app listening on port http://127.0.0.1:${port}!`)
+  console.log(`Example app listening on port http://127.0.0.1:${port}`)
 );
